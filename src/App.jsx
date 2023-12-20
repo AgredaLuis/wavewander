232b9f94ae01a57086bcf552e06e39e805c527e4
AgredaLuis <Luis.Agreda98@gmail.com>
2023-12-20 11:13:45 -0400

    Feat Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-2xl font-bold'>Pagina en construccion</h1>
+      <Navbar />
     </>
   )
 }
